addb34fbbf39cff2c6ed86ee3d10a3f184b05e72
Erona <erona@loli.bz>
2021-08-30 13:21:41 +0800

    fix proxy headers

--- src/matrix-api.js
+++ src/matrix-api.js
@@ -3,12 +3,18 @@ const https = require('https');
 
 const httx = process.env.HOMESERVER_URL.startsWith('https:') ? https : http;
 
+function getHeaders(req) {
+  const h = { ...req.headers };
+  delete h.host;
+  return h;
+}
+
 exports.uploadHandler = function (req, res) {
   const url = `${process.env.HOMESERVER_URL}/_matrix/media/r0/upload`;
   const opts = {
     method: 'POST',
     headers: {
-      ...req.headers,
+      ...getHeaders(req),
       Authorization: 'Bearer ' + process.env.ACCESS_TOKEN,
     },
   };
@@ -21,7 +27,7 @@ exports.uploadHandler = function (req, res) {
 
 exports.thumbnailHandler = function (req, res) {
   req.pipe(httx.request(`${process.env.HOMESERVER_URL}/_matrix/media/r0/thumbnail${req.url.substr(4)}`,
-  { headers: req.headers }, (response) => {
+  { headers: getHeaders(req) }, (response) => {
     res.writeHead(response.statusCode, response.headers);
     response.pipe(res);
   }));
